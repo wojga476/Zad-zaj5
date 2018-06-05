ac964feaad96d82a1a1a5d731deb50fb341f5b76
wojga476 <wojga476@gmail.com>
2018-06-05 15:38:53 +0200

    Zadania zajęcia 5

---
+++ src/LiczParzyste.java
@@ -0,0 +1,16 @@
+public class LiczParzyste {
+
+    public static void main(String[] args) {
+        int liczba = 0;
+
+        for (liczba = 0; liczba < 100; liczba += 2) {
+            System.out.println(liczba);
+//            int suma = liczba+liczba;
+//            System.out.println(suma);
+//                System.out.println("suma liczb parzystych:");
+
+        }
+
+
+    }
+}
